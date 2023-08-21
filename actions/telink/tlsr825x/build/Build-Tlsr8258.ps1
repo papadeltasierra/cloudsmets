25b894c5f167797402a4c2c081b99d231d6443f2
papadeltasierra <paul_d_smith@hotmail.com>
2023-08-21 20:01:11 +0100

    Need quotes around argument list

--- actions/telink/tlsr825x/build/Build-Tlsr8258.ps1
+++ actions/telink/tlsr825x/build/Build-Tlsr8258.ps1
@@ -53,17 +53,17 @@ Write-Information "Running build process '${TelinkIdePath}\eclipsec.exe'..."
 # Time out the build after 5 minutes in case something hangs.
 $proc=Start-Process `
     -FilePath "${TelinkIdePath}\eclipsec.exe" `
-    -ArgumentList `
-    -vm, "${TelinkIdePath}\jre\bin\client", `
-    -noSplash, `
-    -application, "org.eclipse.cdt.managedbuilder.core.headlessbuild", `
-    -import, "${env:GITHUB_WORKSPACE}\${Project}", `
-    -cleanBuild, "tlsr_tc32/${Target}", `
-    --launcher.suppressErrors `
-    -NoNewWindow `
     -RedirectStandardOutput "${env:TEMP}\stdout.txt" `
     -RedirectStandardError "${env:TEMP}\stderr.txt" `
-    -PassThru"
+    -PassThru" `
+    -ArgumentList `
+        "-vm", "${TelinkIdePath}\jre\bin\client", `
+        "-noSplash", `
+        "-application", "org.eclipse.cdt.managedbuilder.core.headlessbuild", `
+        "-import", "${env:GITHUB_WORKSPACE}\${Project}", `
+        "-cleanBuild", "tlsr_tc32/${Target}", `
+        "--launcher.suppressErrors" `
+        "-NoNewWindow"
 
 if ($LASTEXITCODE -ne 0)
 {
