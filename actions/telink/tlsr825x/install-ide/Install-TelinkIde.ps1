dc762025a2795fdad08594814edb77cb98904001
papadeltasierra <paul_d_smith@hotmail.com>
2023-08-22 13:46:36 +0100

    Correct parameter usage.

--- actions/telink/tlsr825x/install-ide/Install-TelinkIde.ps1
+++ actions/telink/tlsr825x/install-ide/Install-TelinkIde.ps1
@@ -6,46 +6,46 @@ Param(
     [Parameter(Mandatory = $true)]
     [string] $TelinkIdePath,
     [Parameter(Mandatory = $true)]
-    [string] $IdeUri,
+    [string] $TelinkIdeUri,
     [Parameter(Mandatory = $true)]
-    [string] $IdeZip,
+    [string] $TelinkIdeZip,
     [Parameter(Mandatory = $true)]
-    [string] $IdeHash
+    [string] $TelinkIdeHash
 )
 
 Write-Information "Creating target directory '${TelinkIdePath}..."
-if (!(Test-Path $TelinkIdePath -PathType container)) {
-    New-Item -Path $TelinkIdePath -ItemType "directory"
+if (!(Test-Path ${TelinkIdePath} -PathType container)) {
+    New-Item -Path ${TelinkIdePath} -ItemType "directory"
 }
 
 Write-Information "Downloading the IDE..."
-if (!(Test-Path "${env:_TEMP}" -PathType container)) {
-    New-Item -Path "${env:_TEMP}" -ItemType "directory"
+if (!(Test-Path "${env:TEMP}" -PathType container)) {
+    New-Item -Path "${env:TEMP}" -ItemType "directory"
 }
-Invoke-RestMethod -Method GET -FollowRelLink -Uri "$IdeUri" -OutFile "${env:_TEMP}\$IdeZip"
+Invoke-RestMethod -Method GET -FollowRelLink -Uri "${TelinkIdeUri}" -OutFile "${env:_TEMP}\${TelinkIdeZip}"
 
 Write-Information "Validating the downloaded IDE..."
-$Hash=Get-FileHash -Path "${env:_TEMP}\$IdeZip" -Algorithm sha256
-if ($Hash.hash.ToString() -ne $env:IDE_HASH)
+$Hash=Get-FileHash -Path "${env:TEMP}\${TelinkIdeZip}" -Algorithm sha256
+if ($Hash.hash.ToString() -ne "${TelinkIdeHash}")
 {
     Write-Error "IDE hash has changed implying IDE update!"
-    Write-Error "Expected sha256: $IdeHash"
+    Write-Error "Expected sha256: ${TelinkIdeHash}"
     Write-Error "Actual sha256:   $(Out-String -InputObject $Hash.hash)"
     exit 1
 }
 
 Write-Information "Unzipping the IDE..."
-Expand-Archive -Path "${env:_TEMP}\$IdeZip" -DestinationPath "${env:_TEMP}"
-run: Remove-Item -Path "${env:_TEMP}\$IdeZip"
+Expand-Archive -Path "${env:TEMP}\${TelinkIdeZip}" -DestinationPath "${env:TEMP}"
+run: Remove-Item -Path "${env:TEMP}\${TelinkIdeZip}"
 if ($DebugPreference == 'Continue')
 {
-    Get-ChildItem -Path "${env:_TEMP}" -Recurse -Name
+    Get-ChildItem -Path "${env:TEMP}" -Recurse -Name
 }
 
 # TODO: Want to hard-code the executable name.
 # TODO: Use the & operator and splat the arguments (or set as list?).
 Write-Information "Installing the IDE..."
-$exes=Get-ChildItem -Path "${env:_TEMP}\IDE\telink*.exe"
+$exes=Get-ChildItem -Path "${env:TEMP}\IDE\telink*.exe"
 # Run the installer, waiting for it to complte, redirecting output etc.
 $proc=Start-Process `
     -FilePath $exes[0].FullName `
@@ -56,18 +56,18 @@ $proc=Start-Process `
     "/NOICONS", `
     "/LOG=""${logfile}""" `
     -NoNewWindow `
-    -RedirectStandardOutput "${env:_TEMP}\stdout.txt" `
-    -RedirectStandardError "${env:_TEMP}\stderr.txt" `
+    -RedirectStandardOutput "${env:TEMP}\stdout.txt" `
+    -RedirectStandardError "${env:TEMP}\stderr.txt" `
     -Wait `
     -PassThru
 
 Write-Information "Installer exit code: $($proc.ExitCode.ToString())."
 Write-Information "STDOUT from the installer..."
-Get-Content -Path ${env:_TEMP}\stdout.txt
+Get-Content -Path ${env:TEMP}\stdout.txt
 Write-Information "STDERR from the installer..."
-Get-Content -Path "${env:_TEMP}\stderr.txt"
-Remove-Item -Path "${env:_TEMP}\stdout.txt"
-Remove-Item -Path "${env:_TEMP}\stderr.txt"
+Get-Content -Path "${env:TEMP}\stderr.txt"
+Remove-Item -Path "${env:TEMP}\stdout.txt"
+Remove-Item -Path "${env:TEMP}\stderr.txt"
 
 if ($DebugPreference == 'Continue')
 {
