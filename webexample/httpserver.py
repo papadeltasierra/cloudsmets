ece2a7d0a19e415573ca901ca993a3a8afd228f9
Paul D.Smith <paul@pauldsmith.org.uk>
2023-11-12 18:12:54 +0000

    Starting to create simple menus

---
+++ webexample/httpserver.py
@@ -0,0 +1,11 @@
+# simple server for testing purposes.
+import http.server
+import socketserver
+
+PORT = 8000
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
