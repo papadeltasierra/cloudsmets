2e6d781f999f8345a19d507f6df376f75724c984
Paul D.Smith <paul@pauldsmith.org.uk>
2023-11-17 10:03:36 +0000

    Add OTA

--- webexample/minihtml.py
+++ webexample/minihtml.py
@@ -5,6 +5,7 @@ import re
 
 RGX_JS = re.compile(r"""(\.js["'])""")
 RGX_CSS = re.compile(r"""(\.css["'])""")
+RGX_HTML0 = re.compile(r"""\.htm(["'])""")
 RGX_HTML1 = re.compile(r">\s+<")
 RGX_HTML2 = re.compile(r">\s+")
 RGX_HTML3 = re.compile(r"\s+<")
@@ -18,10 +19,17 @@ for rootdir, directories, filenames in os.walk(sys.argv[1]):
             with open(fullpath, "r") as source:
                 html = source.read()
 
+            # Add the "-min" to the name of JS files
             html = RGX_JS.sub(r"-min\1", html)
+            # Add the "-min" to the name of CSS files
             html = RGX_CSS.sub(r"-min\1", html)
+            # Rename "".htm" files to "".html".
+            html = RGX_HTML0.sub(r".html\1", html)
+            # Remove space betwen HTML items.
             html = RGX_HTML1.sub(r"><", html)
+            # Remove leading space in strings.
             html = RGX_HTML2.sub(r">", html)
+            # Remove trailing space from stings.
             html = RGX_HTML3.sub(r"<", html)
 
             with open(fullpath, "w") as target:
