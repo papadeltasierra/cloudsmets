fb7c6596a9a76bd25a82469afac8a773ec1718ba
Paul D.Smith <paul@pauldsmith.org.uk>
2023-11-28 21:08:54 +0000

    remote logging updates.

---
+++ esp32/cloudsmets/main/debug.c
@@ -0,0 +1,11 @@
+
+
+sock_init();
+
+/*
+ * Note that this code has to be reentrant i.e. can be simultaneously called
+ * from multiple threads.
+ * */
+int sock_vprintf(const char *fmt, va_list args)
+{
+}
